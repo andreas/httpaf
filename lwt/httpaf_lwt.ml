b6f91b03c9e58f12845b93a892676446ca6336e0
Andreas Garnaes <andreas.garnaes@gmail.com>
2019-01-18 15:20:56 +0100

    wip

--- lwt/httpaf_lwt.ml
+++ lwt/httpaf_lwt.ml
@@ -74,100 +74,188 @@ let shutdown socket command =
 
 module Config = Httpaf.Config
 
+module type Protocol = sig
+  open Httpaf
+
+  type t
+
+  val next_read_operation : t -> [ `Read | `Yield | `Close ]
+
+  val read : t -> Bigstring.t -> off:int -> len:int -> int
+
+  val read_eof : t -> Bigstring.t -> off:int -> len:int -> int
+
+  val yield_reader : t -> (unit -> unit) -> unit
+
+  val next_write_operation : t -> [
+    | `Write of Bigstring.t IOVec.t list
+    | `Yield
+    | `Close of int ]
+
+  val report_write_result : t -> [`Ok of int | `Closed] -> unit
+
+  val yield_writer : t -> (unit -> unit) -> unit
+
+  val report_exn : t -> exn -> unit
+end
+
+type 'a protocol = (module Protocol with type t = 'a)
+
+module Upgradable_reqd = struct
+  open Httpaf
+
+  type t = T : {
+    reqd    : Reqd.t;
+    upgrade : 'a protocol -> 'a -> unit;
+  } -> t
+
+  let create reqd upgrade =
+    T { reqd; upgrade }
+end
+
+module Upgradable_connection = struct
+  open Httpaf
+
+  type io_handler = IOHandler : 'a protocol * 'a -> io_handler
+
+  type t = {
+    mutable io_handler : io_handler;
+  }
+
+  let upgrade_protocol t protocol' t' =
+    t.io_handler <- IOHandler (protocol', t')
+
+  let create ?config ?error_handler request_handler =
+    let rec t = lazy {
+      io_handler = IOHandler ((module Server_connection), Server_connection.create ?config ?error_handler request_handler')
+    }
+    and request_handler' reqd =
+      let reqd' = Upgradable_reqd.create reqd (upgrade_protocol Lazy.(force t)) in
+      request_handler reqd'
+    in
+    Lazy.force t
+
+  let next_read_operation { io_handler = IOHandler((module P), t) } =
+    P.next_read_operation t
+
+  let read { io_handler = IOHandler((module P), t) } =
+    P.read t
+
+  let read_eof { io_handler = IOHandler((module P), t) } =
+    P.read_eof t
+
+  let yield_reader { io_handler = IOHandler((module P), t) } =
+    P.yield_reader t
+
+  let next_write_operation { io_handler = IOHandler((module P), t) } =
+    P.next_write_operation t
+
+  let report_write_result { io_handler = IOHandler((module P), t) } =
+    P.report_write_result t
+
+  let yield_writer { io_handler = IOHandler((module P), t) } =
+    P.yield_writer t
+
+  let report_exn { io_handler = IOHandler((module P), t) } =
+    P.report_exn t
+end
+
 module Server = struct
+  let server_loop connection socket ~read_buffer =
+    let read_loop_exited, notify_read_loop_exited = Lwt.wait () in
+
+    let rec read_loop () =
+      let rec read_loop_step () =
+        match Upgradable_connection.next_read_operation connection with
+        | `Read ->
+          read socket read_buffer >>= begin function
+          | `Eof ->
+            Buffer.get read_buffer ~f:(fun bigstring ~off ~len ->
+              Upgradable_connection.read_eof connection bigstring ~off ~len)
+            |> ignore;
+            read_loop_step ()
+          | `Ok _ ->
+            Buffer.get read_buffer ~f:(fun bigstring ~off ~len ->
+              Upgradable_connection.read connection bigstring ~off ~len)
+            |> ignore;
+            read_loop_step ()
+          end
+
+        | `Yield ->
+          Upgradable_connection.yield_reader connection read_loop;
+          Lwt.return_unit
+
+        | `Close ->
+          Lwt.wakeup_later notify_read_loop_exited ();
+          if not (Lwt_unix.state socket = Lwt_unix.Closed) then begin
+            shutdown socket Unix.SHUTDOWN_RECEIVE
+          end;
+          Lwt.return_unit
+      in
+
+      Lwt.async (fun () ->
+        Lwt.catch
+          read_loop_step
+          (fun exn ->
+            Upgradable_connection.report_exn connection exn;
+            Lwt.return_unit))
+    in
+
+
+    let writev = Faraday_lwt_unix.writev_of_fd socket in
+    let write_loop_exited, notify_write_loop_exited = Lwt.wait () in
+
+    let rec write_loop () =
+      let rec write_loop_step () =
+        match Upgradable_connection.next_write_operation connection with
+        | `Write io_vectors ->
+          writev io_vectors >>= fun result ->
+          Upgradable_connection.report_write_result connection result;
+          write_loop_step ()
+
+        | `Yield ->
+          Upgradable_connection.yield_writer connection write_loop;
+          Lwt.return_unit
+
+        | `Close _ ->
+          Lwt.wakeup_later notify_write_loop_exited ();
+          if not (Lwt_unix.state socket = Lwt_unix.Closed) then begin
+            shutdown socket Unix.SHUTDOWN_SEND
+          end;
+          Lwt.return_unit
+      in
+
+      Lwt.async (fun () ->
+        Lwt.catch
+          write_loop_step
+          (fun exn ->
+            Upgradable_connection.report_exn connection exn;
+            Lwt.return_unit))
+    in
+
+
+    read_loop ();
+    write_loop ();
+    Lwt.join [read_loop_exited; write_loop_exited] >>= fun () ->
+
+    if Lwt_unix.state socket <> Lwt_unix.Closed then
+      Lwt.catch
+        (fun () -> Lwt_unix.close socket)
+        (fun _exn -> Lwt.return_unit)
+    else
+      Lwt.return_unit
+
   let create_connection_handler ?(config=Config.default) ~request_handler ~error_handler =
     fun client_addr socket ->
       let module Server_connection = Httpaf.Server_connection in
       let connection =
-        Server_connection.create
+        Upgradable_connection.create
           ~config
           ~error_handler:(error_handler client_addr)
           (request_handler client_addr)
       in
-
       let read_buffer = Buffer.create config.read_buffer_size in
-      let read_loop_exited, notify_read_loop_exited = Lwt.wait () in
-
-      let rec read_loop () =
-        let rec read_loop_step () =
-          match Server_connection.next_read_operation connection with
-          | `Read ->
-            read socket read_buffer >>= begin function
-            | `Eof ->
-              Buffer.get read_buffer ~f:(fun bigstring ~off ~len ->
-                Server_connection.read_eof connection bigstring ~off ~len)
-              |> ignore;
-              read_loop_step ()
-            | `Ok _ ->
-              Buffer.get read_buffer ~f:(fun bigstring ~off ~len ->
-                Server_connection.read connection bigstring ~off ~len)
-              |> ignore;
-              read_loop_step ()
-            end
-
-          | `Yield ->
-            Server_connection.yield_reader connection read_loop;
-            Lwt.return_unit
-
-          | `Close ->
-            Lwt.wakeup_later notify_read_loop_exited ();
-            if not (Lwt_unix.state socket = Lwt_unix.Closed) then begin
-              shutdown socket Unix.SHUTDOWN_RECEIVE
-            end;
-            Lwt.return_unit
-        in
-
-        Lwt.async (fun () ->
-          Lwt.catch
-            read_loop_step
-            (fun exn ->
-              Server_connection.report_exn connection exn;
-              Lwt.return_unit))
-      in
-
-
-      let writev = Faraday_lwt_unix.writev_of_fd socket in
-      let write_loop_exited, notify_write_loop_exited = Lwt.wait () in
-
-      let rec write_loop () =
-        let rec write_loop_step () =
-          match Server_connection.next_write_operation connection with
-          | `Write io_vectors ->
-            writev io_vectors >>= fun result ->
-            Server_connection.report_write_result connection result;
-            write_loop_step ()
-
-          | `Yield ->
-            Server_connection.yield_writer connection write_loop;
-            Lwt.return_unit
-
-          | `Close _ ->
-            Lwt.wakeup_later notify_write_loop_exited ();
-            if not (Lwt_unix.state socket = Lwt_unix.Closed) then begin
-              shutdown socket Unix.SHUTDOWN_SEND
-            end;
-            Lwt.return_unit
-        in
-
-        Lwt.async (fun () ->
-          Lwt.catch
-            write_loop_step
-            (fun exn ->
-              Server_connection.report_exn connection exn;
-              Lwt.return_unit))
-      in
-
-
-      read_loop ();
-      write_loop ();
-      Lwt.join [read_loop_exited; write_loop_exited] >>= fun () ->
-
-      if Lwt_unix.state socket <> Lwt_unix.Closed then
-        Lwt.catch
-          (fun () -> Lwt_unix.close socket)
-          (fun _exn -> Lwt.return_unit)
-      else
-        Lwt.return_unit
+      server_loop connection socket ~read_buffer
 end
 
 
--- lwt/httpaf_lwt.mli
+++ lwt/httpaf_lwt.mli
@@ -1,5 +1,33 @@
 open Httpaf
 
+module type Protocol = sig
+  type t
+
+  val next_read_operation : t -> [ `Read | `Yield | `Close ]
+
+  val read : t -> Bigstring.t -> off:int -> len:int -> int
+
+  val read_eof : t -> Bigstring.t -> off:int -> len:int -> int
+
+  val yield_reader : t -> (unit -> unit) -> unit
+
+  val next_write_operation : t -> [
+    | `Write of Bigstring.t IOVec.t list
+    | `Yield
+    | `Close of int ]
+
+  val report_write_result : t -> [`Ok of int | `Closed] -> unit
+
+  val yield_writer : t -> (unit -> unit) -> unit
+
+  val report_exn : t -> exn -> unit
+end
+
+type 'a protocol = (module Protocol with type t = 'a)
+
+module Upgradable_reqd : sig
+  type t
+end
 
 (* The function that results from [create_connection_handler] should be passed
    to [Lwt_io.establish_server_with_client_socket]. For an example, see
@@ -7,7 +35,7 @@ open Httpaf
 module Server : sig
   val create_connection_handler
     :  ?config         : Config.t
-    -> request_handler : (Unix.sockaddr -> Server_connection.request_handler)
+    -> request_handler : (Unix.sockaddr -> Upgradable_reqd.t -> unit)
     -> error_handler   : (Unix.sockaddr -> Server_connection.error_handler)
     -> Unix.sockaddr
     -> Lwt_unix.file_descr
